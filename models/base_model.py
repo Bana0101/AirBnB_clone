79a29c7c0505b90d2b769b1560e389cdbd492a93
xtreme <anastaai94@gmail.com>
2024-01-12 20:06:06 +0100

    ba9i output mgloub

--- models/base_model.py
+++ models/base_model.py
@@ -5,19 +5,18 @@ import datetime, uuid
 class BaseModel:
     """ the basemodel class """
 
-    id = None
+    id = str(uuid.uuid4())
     created_at = None
     updated_at = None
 
-    def __init__(self, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-        self.save()
-
     def save(self):
         self.updated_at = datetime.datetime.now().isoformat()
+
+    def __init__(self, *args, **kwargs):
+        self.created_at = datetime.datetime.now().isoformat()
+        self.save()
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
@@ -25,5 +24,8 @@ class BaseModel:
     def to_dict(self):
         dic = {"__class__": self.__class__.__name__}
         for key, value in self.__dict__.items():
-            dic.update({key: value})
+            if key in ["created_at", "updated_at"] and isinstance(value, datetime.datetime):
+                dic[key] = value.isoformat()
+            else:
+                dic[key] = value
         return dic
