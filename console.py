74c44990e5308dc709d0f8a048a21126b46d64f4
Ayoub <sb.naba2@gmail.com>
2024-01-14 14:10:11 -0800

    the console

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" import documentation """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Hbnb documentation"""
+    prompt = '(hbnb) '
+
+
+    def do_quit(self, arg):
+        """ Exist the program"""
+        return True
+
+
+    def do_EOF(self, arg):
+        """ Exist the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        """ Do nothing on empty iput line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
